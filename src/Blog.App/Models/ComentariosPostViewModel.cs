97ffcf865404d9702c34e8ba0602ed3b2584d6a3
HugoNunes <hugo_nunes@outlook.com>
2024-11-06 00:19:11 -0300

    Reaplicando configurações de EOL

---
+++ src/Blog.App/Models/ComentariosPostViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.App.Models
+{
+    public class ComentariosPostViewModel
+    {
+        public PostViewModel Post { get; set; }
+        public IEnumerable<ComentarioViewModel> Comentarios { get; set; }
+    }
+}
