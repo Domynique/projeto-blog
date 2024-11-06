1b875ff0ca4f47d34f7cd49e6f191749f79184ab
Hugo Domynique R. Nunes <44654088+Domynique@users.noreply.github.com>
2024-11-05 21:01:31 -0300

    Delete src/Blog.Web directory

--- src/Blog.App/Program.cs
+++ src/Blog.App/Program.cs
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-using Blog.App.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-=======
 using Blog.App.Configurations;
 using Blog.Core.Data.Configurations;
 
@@ -31,25 +13,18 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddAutoMapperConfig();
 
->>>>>>> 7b94a7c (Ajustes projeto BlogApp)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-<<<<<<< HEAD
-=======
+
     app.UseDeveloperExceptionPage();
->>>>>>> 7b94a7c (Ajustes projeto BlogApp)
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-<<<<<<< HEAD
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-=======
->>>>>>> 7b94a7c (Ajustes projeto BlogApp)
     app.UseHsts();
 }
 
@@ -65,9 +40,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-<<<<<<< HEAD
-=======
 app.UseDbMigrationHelper();
 
->>>>>>> 7b94a7c (Ajustes projeto BlogApp)
 app.Run();
